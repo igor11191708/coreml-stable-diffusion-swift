a2eac18cd55c320e63d8759ccd7dff08438b4653
Igor <igorshelopaev@gmail.com>
2023-03-24 10:16:45 +0500

    comments

--- Sources/coreml-stable-diffusion-swift/model/GenerativeModel.swift
+++ Sources/coreml-stable-diffusion-swift/model/GenerativeModel.swift
@@ -8,18 +8,22 @@
 import Foundation
 
 @available(iOS 16.2, macOS 13.1, *)
+/// Model  profile description
 public struct GenerativeModel: Hashable, Identifiable{
     
+    // MARK: - Life circle
     public init(url: URL? = nil, name: String) {
         self.url = url
         self.name = name
     }
     
-    
+    /// Identifier
     public let id = UUID()
     
+    /// The path to the models files
     public let url: URL?
-    
+        
+    /// Name of the model
     public let name: String
     
 }
