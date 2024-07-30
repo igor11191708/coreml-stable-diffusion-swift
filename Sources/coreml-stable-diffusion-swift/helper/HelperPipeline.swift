44749d6ab332c7c0567517c1e2372d5c675c152a
Igor <igorshelopaev@gmail.com>
2024-07-30 12:59:02 +0200

    Update HelperPipeline.swift

--- Sources/coreml-stable-diffusion-swift/helper/HelperPipeline.swift
+++ Sources/coreml-stable-diffusion-swift/helper/HelperPipeline.swift
@@ -9,29 +9,37 @@ import Foundation
 import StableDiffusion
 import CoreML
 
-/// Get diffusion pipeline
+/// Retrieves a StableDiffusionPipeline configured with the specified parameters.
+///
 /// - Parameters:
-///   - url: The path to the models files
-///   - disableSafety: Disable safety check
-///   - reduceMemory: Reduce memory if you use it on devises with limited memory amount
-///   - computeUnits: The set of processing-unit configurations the model can use to make predictions
-/// - Throws: Errors while creating pipeline
-/// - Returns: A pipeline used to generate image samples from text input using stable diffusion
+///   - url: The URL where the pipeline resources are located.
+///   - disableSafety: A boolean flag to disable safety checks. Default is `false`.
+///   - reduceMemory: A boolean flag to reduce memory usage. Default is `false`.
+///   - computeUnits: The compute units to be used by the pipeline. Default is `.cpuAndGPU`.
+///
+/// - Throws: An error if the pipeline initialization fails.
+///
+/// - Returns: A configured instance of `StableDiffusionPipeline`.
+
 @available(iOS 16.2, macOS 13.1, *)
 public func getDiffusionPipeline(
     for url : URL,
     _ disableSafety : Bool = false,
     _ reduceMemory : Bool = false,
-    _ computeUnits : MLComputeUnits = .cpuAndNeuralEngine
-) throws -> StableDiffusionPipeline{
+    _ computeUnits : MLComputeUnits = .cpuAndGPU
+) throws -> StableDiffusionPipeline {
     
+    // Initialize the MLModelConfiguration with the specified compute units.
     let config: MLModelConfiguration = .init()
     config.computeUnits = computeUnits
     
+    // Return a new StableDiffusionPipeline instance, using the provided URL for resources.
+    // The pipeline is configured with optional parameters to disable safety checks and reduce memory usage.
     return try .init(
-        resourcesAt: url, controlNet: [],
-        configuration : config,
-        disableSafety: disableSafety,
-        reduceMemory : reduceMemory
+        resourcesAt: url, // The URL where the pipeline resources are located.
+        controlNet: [],   // An empty array for controlNet as no control network is being passed.
+        configuration: config, // The MLModelConfiguration object with the compute units setting.
+        disableSafety: disableSafety, // A boolean flag to disable safety checks if set to true.
+        reduceMemory: reduceMemory // A boolean flag to reduce memory usage if set to true.
     )
 }
